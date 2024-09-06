294f6a6ded6476e8e4677b4b6a2948686b39626c
AOKIHIROHARAF <143846961+AOKIHIROHARAF@users.noreply.github.com>
2024-09-06 14:49:52 -0500

    restructure the baseball directory and update  scrape_data.py with annotations

--- data/baseball/scrape_data.py
+++ data/baseball/scrape_data.py
@@ -3,17 +3,18 @@ from bs4 import BeautifulSoup
 import csv
 import os
 
+# define the function for scraping data with a given url
 def scrape_data(url):
     response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        table = soup.find('table')
+    if response.status_code == 200:  # request successful
+        soup = BeautifulSoup(response.content, 'html.parser')  # parse the html content of website using parser
+        table = soup.find('table')  # get the <table> element
         
         if table:
-            headers = [th.text.strip() for th in table.find_all('th')]
+            headers = [th.text.strip() for th in table.find_all('th')]  # extract headers <th>
             rows = []
-            for tr in table.find_all('tr')[1:]:  # Skip header
-                row = [td.text.strip() for td in tr.find_all('td')]
+            for tr in table.find_all('tr')[1:]:  # go over each row, excluding the first one(header)
+                row = [td.text.strip() for td in tr.find_all('td')]  # extracting the cells <td>
                 rows.append(row)
             return headers, rows
         else:
@@ -24,9 +25,10 @@ def scrape_data(url):
         return None, None
 
 years = range(12, 24)
-directory = './data/baseball'  
 
-os.makedirs(directory, exist_ok=True)
+directory = './data/baseball'  # the directory to store the data
+
+os.makedirs(directory, exist_ok=True)   # check if this directory exists
 
 for year in years:
     al_url = f'https://bbwaa.com/{year}-al-mvp-ballots/'
@@ -34,8 +36,8 @@ for year in years:
     
     al_headers, al_rows = scrape_data(al_url)
     if al_headers and al_rows:
-        al_file_name = os.path.join(directory, f'al_mvp_ballots_{year}.csv')
-        with open(al_file_name, mode='w', newline='', encoding='utf-8') as al_file:
+        al_file_name = os.path.join(directory, f'al_mvp_ballots_{year}.csv')   # construct the output file path
+        with open(al_file_name, mode='w', newline='', encoding='utf-8') as al_file:   # set the mode to write
             al_writer = csv.writer(al_file)
             al_writer.writerow(al_headers) 
             al_writer.writerows(al_rows) 
